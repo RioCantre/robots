158242eb8c013a21819ac6f986c9752ba18a3772
Rio Cantre <smittenaf301icu@gmail.com>
2021-09-21 18:37:40 +0200

    Added card,robot files and tachyons

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Card from './card';
+import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>,
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+  </div>,
   document.getElementById('root')
 );
 
@@ -14,4 +19,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
