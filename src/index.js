3f56400e3402022fb65acbe863889ebf875eed28
Rio Cantre <smittenaf301icu@gmail.com>
2021-09-21 22:17:48 +0200

    Added SearchBox and App

--- src/index.js
+++ src/index.js
@@ -2,16 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import CardList from './cardList';
+import App from './App';
 import 'tachyons';
-import { robots } from './robots';
 
-ReactDOM.render(
-  <div>
-    <CardList robots={robots} />
-  </div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
